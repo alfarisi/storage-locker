eebb99ec3acd39942c9a52fb0eb50514ff168765
alfarisi <farisi_al@yahoo.com>
2019-06-28 18:49:06 +0700

    updated test, removed error lib

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -23,149 +22,176 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 		}
 		
-		err = runCommand(cmdString)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		isSuccess, msg := runCommand(cmdString)
+		if isSuccess == false {
+			fmt.Fprintln(os.Stderr, msg)
 		}
 	}
 }
 
-func runCommand(commandStr string) error {
+func runCommand(commandStr string) (bool, string) {
+	var msg string
 	commandStr = strings.TrimSuffix(commandStr, "\n")	
 	arrCommandStr := strings.Fields(commandStr)
 	
 	if len(arrCommandStr) < 1 {
-		return errors.New("Mohon tuliskan perintah..")
+		msg = "Mohon tuliskan perintah.."
+		return false, msg
 	} else if (arrCommandStr[0] != "init") && (arrCommandStr[0] != "help") && (arrCommandStr[0] != "exit") && (nLocker == 0) {
-		return errors.New("Mohon tentukan jumlah locker terlebih dulu dengan perintah init [n].\n")
+		msg = "Mohon tentukan jumlah locker terlebih dulu dengan perintah init [n].\n"
+		return false, msg
 	}
 	
 	switch arrCommandStr[0] {
-		case "exit":
-			os.Exit(0)
+	case "exit":
+		os.Exit(0)
+	
+	case "init":
+		if nLocker != 0 {
+			msg = fmt.Sprintf("Jumlah locker sudah ditentukan sebelumnya : %d \n", nLocker)
+			return false, msg
+		}
 		
-		case "init":
-			if nLocker != 0 {
-				msg := fmt.Sprintf("Jumlah locker sudah ditentukan sebelumnya : %d \n", nLocker)
-				return errors.New(msg)
-			}
-			
-			if len(arrCommandStr) < 2 {
-				return errors.New("Perintah ini memerlukan 1 argument : [jumlah locker]")
-			}
-			
-			n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
-			
-			if (err != nil) || (n == 0) {
-				return errors.New("Error: jumlah locker harus bilangan positif dari 1 sampai 255")
-			}
-			
-			nLocker = uint8(n)
-			lockers = make([]map[string]string, nLocker)
-			
-			fmt.Fprintln(os.Stdout, "Berhasil membuat locker dengan jumlah", nLocker)
-			return nil
+		if len(arrCommandStr) < 2 {
+			msg = "Perintah ini memerlukan 1 argument : [jumlah_locker]"
+			return false, msg
+		}
 		
-		case "input":
-			if len(arrCommandStr) < 3 {
-				return errors.New("Perintah ini memerlukan 2 argument : [KTP|SIM|Other] [nomor ID]")
-			} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
-				return errors.New("Error: Tipe identitas yang dapat digunakan hanya KTP, SIM atau Other.\n")
-			}
-			
-			tLockerNum := findNomorIdentitas(arrCommandStr[2])
-			if tLockerNum != 0 {
-				msg := fmt.Sprintf("Peringatan: Kartu identitas dengan nomor %s sebelumnya sudah tersimpan di locker nomor %d \n", arrCommandStr[2], tLockerNum)
-				return errors.New(msg)
-			}
-			
-			lockerNum := inputLocker(arrCommandStr[1], arrCommandStr[2])
-			
-			if lockerNum == 0 {
-				return errors.New("Maaf locker sudah penuh.\n")
+		n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
+		
+		if (err != nil) || (n == 0) {
+			msg = "Error: jumlah locker harus bilangan positif dari 1 sampai 255"
+			return false, msg
+		}
+		
+		nLocker = uint8(n)
+		lockers = make([]map[string]string, nLocker)
+		
+		msg = "Success"
+		fmt.Fprintln(os.Stdout, "Berhasil membuat locker dengan jumlah", nLocker)
+		return true, msg
+	
+	case "input":
+		if len(arrCommandStr) < 3 {
+			msg = "Perintah ini memerlukan 2 argument : [KTP|SIM|Other] [nomor ID]"
+			return false, msg
+		} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
+			msg = "Error: Tipe identitas yang dapat digunakan hanya KTP, SIM atau Other.\n"
+			return false, msg
+		}
+		
+		tLockerNum := findNomorIdentitas(arrCommandStr[2])
+		if tLockerNum != 0 {
+			msg = fmt.Sprintf("Peringatan: Kartu identitas dengan nomor %s sebelumnya sudah tersimpan di locker nomor %d \n", arrCommandStr[2], tLockerNum)
+			return false, msg
+		}
+		
+		lockerNum := inputLocker(arrCommandStr[1], arrCommandStr[2])
+		
+		if lockerNum == 0 {
+			msg = "Maaf locker sudah penuh.\n"
+			return false, msg
+		} else {
+			msg = "Success"
+			fmt.Fprintln(os.Stdout, "Kartu identitas tersimpan di locker nomor", lockerNum)
+			return true, msg
+		}
+	
+	case "status":
+		fmt.Fprintln(os.Stdout, "No. Locker   Tipe Identitas   No. Identitas")
+		
+		for k, val := range lockers {
+			if (val == nil) || (val["status"] == "kosong") {
+				row := fmt.Sprintf("%9d.", k+1)
+				fmt.Fprintln(os.Stdout, row)
 			} else {
-				fmt.Fprintln(os.Stdout, "Kartu identitas tersimpan di locker nomor", lockerNum)
-				return nil
+				row := fmt.Sprintf("%9d.   %-14s   %s", k+1, val["tipeId"], val["nomorId"])
+				fmt.Fprintln(os.Stdout, row)
 			}
+		}
 		
-		case "status":
-			fmt.Fprintln(os.Stdout, "No. Locker   Tipe Identitas   No. Identitas")
-			
-			for k, val := range lockers {
-				if (val == nil) || (val["status"] == "kosong") {
-					row := fmt.Sprintf("%9d.", k+1)
-					fmt.Fprintln(os.Stdout, row)
-				} else {
-					row := fmt.Sprintf("%9d.   %-14s   %s", k+1, val["tipeId"], val["nomorId"])
-					fmt.Fprintln(os.Stdout, row)
-				}
-			}
-			
-			return nil
+		msg = "Success"
+		return true, msg
+	
+	case "leave":
+		if len(arrCommandStr) < 2 {
+			msg = "Perintah ini memerlukan 1 argument : [nomor locker]"
+			return false, msg
+		}
 		
-		case "leave":
-			if len(arrCommandStr) < 2 {
-				return errors.New("Perintah ini memerlukan 1 argument : [nomor locker]")
-			}
-			
-			n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
-			nl := uint8(n)
-			
-			if (err != nil) || (nl == 0) || (nl > nLocker) {
-				return errors.New("Error: Nomor locker tidak tersedia.\n")
-			}
-			
-			lockers[nl-1] = map[string]string{"tipeId": "", "nomorId": "", "status": "kosong"}
-			fmt.Fprintln(os.Stdout, "Loker nomor", n, "berhasil dikosongkan.")
-			return nil
+		n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
+		nl := uint8(n)
 		
-		case "find":
-			if len(arrCommandStr) < 2 {
-				return errors.New("Perintah ini memerlukan 1 argument : [nomor identitas]")
-			}
-			
-			tLockerNum := findNomorIdentitas(arrCommandStr[1])
-			
-			if tLockerNum != 0 {
-				fmt.Fprintln(os.Stdout, "Kartu identitas tersebut berada di locker nomor", tLockerNum)
-				return nil
-			}
-			return errors.New("Nomor identitas tidak ditemukan.\n")
+		if (err != nil) || (nl == 0) || (nl > nLocker) {
+			msg = "Error: Nomor locker tidak tersedia.\n"
+			return false, msg
+		}
 		
-		case "search":
-			if len(arrCommandStr) < 2 {
-				return errors.New("Perintah ini memerlukan 1 argument : [tipe identitas]")
-			} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
-				return errors.New("Tipe identitas yang dapat digunakan hanya KTP, SIM dan Other")
-			}
-			
-			arrNomorId := []string{}
-			isFound := false
-			
-			for _, val := range lockers {
-				if (val["tipeId"] == arrCommandStr[1]) && (val["status"] == "terisi") {
-					arrNomorId = append(arrNomorId, val["nomorId"])
+		lockers[nl-1] = map[string]string{"tipeId": "", "nomorId": "", "status": "kosong"}
+		msg = "Success"
+		fmt.Fprintln(os.Stdout, "Loker nomor", n, "berhasil dikosongkan.")
+		return true, msg
+	
+	case "find":
+		if len(arrCommandStr) < 2 {
+			msg = "Perintah ini memerlukan 1 argument : [nomor identitas]"
+			return false, msg
+		}
+		
+		tLockerNum := findNomorIdentitas(arrCommandStr[1])
+		
+		if tLockerNum != 0 {
+			msg = "Found"
+			fmt.Fprintln(os.Stdout, "Kartu identitas tersebut berada di locker nomor", tLockerNum)
+		} else {
+			msg = "Not Found"
+			fmt.Fprintln(os.Stdout, "Nomor identitas tidak ditemukan.")
+		}
+		return true, msg
+	
+	case "search":
+		if len(arrCommandStr) < 2 {
+			msg = "Perintah ini memerlukan 1 argument : [tipe identitas]"
+			return false, msg
+		} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
+			msg = "Tipe identitas yang dapat digunakan hanya KTP, SIM dan Other"
+			return false, msg
+		}
+		
+		arrNomorId := []string{}
+		isFound := false
+		
+		for _, val := range lockers {
+			if (val["tipeId"] == arrCommandStr[1]) && (val["status"] == "terisi") {
+				arrNomorId = append(arrNomorId, val["nomorId"])
+				if isFound == false {
 					isFound = true
 				}
 			}
-			
-			if isFound {
-				strNomorId := strings.Join(arrNomorId, ", ")
-				fmt.Fprintln(os.Stdout, "Nomor identitas yang tersimpan:", strNomorId)
-			} else {
-				fmt.Fprintln(os.Stdout, "Tidak ditemukan Nomor Identitas dengan tipe", arrCommandStr[1])
-			}
-			return nil
-			
-		case "help":
-			fmt.Fprintln(os.Stdout, "Daftar perintah yang bisa digunakan:\n\n init [jumlah_locker]\n status\n input [KTP|SIM|Other] [nomor_identitas]\n leave [nomor_locker]\n find [nomor_identitas]\n search [KTP|SIM|Other]\n help\n exit")
-			return nil
-			
-		default:
-			return errors.New("Perintah tidak dikenali. Gunakan 'help' untuk melihat daftar perintah yang dapat digunakan.\n")
+		}
+		
+		if isFound {
+			strNomorId := strings.Join(arrNomorId, ", ")
+			msg = "Found"
+			fmt.Fprintln(os.Stdout, "Nomor identitas yang tersimpan:", strNomorId)
+		} else {
+			msg = "Not Found"
+			fmt.Fprintln(os.Stdout, "Tidak ditemukan Nomor Identitas dengan tipe", arrCommandStr[1])
+		}
+		return true, msg
+		
+	case "help":
+		fmt.Fprintln(os.Stdout, "Daftar perintah yang bisa digunakan:\n\n init [jumlah_locker]\n status\n input [KTP|SIM|Other] [nomor_identitas]\n leave [nomor_locker]\n find [nomor_identitas]\n search [KTP|SIM|Other]\n help\n exit")
+		msg = "Success"
+		return true, msg
+		
+	default:
+		msg = "Perintah tidak dikenali. Gunakan 'help' untuk melihat daftar perintah yang dapat digunakan.\n"
+		return false, msg
 	}
 	
-	return nil
+	msg = "Success"
+	return true, msg
 }
 
 func findNomorIdentitas(nomor string) uint8 {
--- main_test.go
+++ main_test.go
@@ -6,41 +6,78 @@ import (
 )
 
 func TestRunCommand(t *testing.T) {
-	cmdString := "status"
-	err := runCommand(cmdString)
-	if err == nil {
-		t.Error("unexpected results: seharusnya tidak bisa menggunakan perintah lain sebelum dilakukan init.")
+	var (
+		isSuccess bool
+		msg string
+	)
+	
+	isSuccess, msg = runCommand("status")
+	if isSuccess == true {
+		t.Error("unexpected results: seharusnya tidak boleh menggunakan perintah 'status' atau lainnya sebelum dilakukan init.")
 	} else {
-		fmt.Println("Perintah 'status' tidak bisa dieksekusi : OK")
+		fmt.Println("[OK]", msg)
 	}
 	
-	cmdString = "init 5"
-	err = runCommand(cmdString)
-	if err != nil {
-		t.Error("unexpected results:", err)
+	isSuccess, msg = runCommand("init 3")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
 	}
 	
-	cmdString = "input KTP 12345"
-	err = runCommand(cmdString)
-	if err != nil {
-		t.Error("unexpected results:", err)
+	isSuccess, msg = runCommand("doAnything")
+	if isSuccess == true {
+		t.Error("unexpected results: seharusnya tidak bisa mengeksekusi perintah 'doAnything'.")
+	} else {
+		fmt.Println("[OK]", msg)
 	}
 	
-	cmdString = "input SIM 34567"
-	err = runCommand(cmdString)
-	if err != nil {
-		t.Error("unexpected results:", err)
+	isSuccess, msg = runCommand("input KTP 12345")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
 	}
 	
-	cmdString = "input Other 95445"
-	err = runCommand(cmdString)
-	if err != nil {
-		t.Error("unexpected results:", err)
+	isSuccess, msg = runCommand("input KTPx 12345032423943")
+	if isSuccess == true {
+		t.Error("unexpected results: seharusnya tidak bisa menggunakan tipe identitas 'KTPx'.")
+	} else {
+		fmt.Println("[OK]", msg)
 	}
 	
-	cmdString = "status"
-	err = runCommand(cmdString)
-	if err != nil {
-		t.Error("unexpected results:", err)
+	isSuccess, msg = runCommand("input KTP 12345")
+	if isSuccess == true {
+		t.Error("unexpected results: seharusnya tidak bisa menginput nomor identitas yang sama.")
+	} else {
+		fmt.Println("[OK]", msg)
+	}
+	
+	isSuccess, msg = runCommand("input SIM 34567")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
+	}
+	
+	isSuccess, msg = runCommand("input Other 95445")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
+	}
+	
+	isSuccess, msg = runCommand("input KTP 92341")
+	if isSuccess == true {
+		t.Error("unexpected results: seharusnya locker sudah penuh.")
+	} else {
+		fmt.Println("[OK]", msg)
+	}
+	
+	isSuccess, msg = runCommand("find 95445")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
+	}
+	
+	isSuccess, msg = runCommand("search KTP")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
+	}
+	
+	isSuccess, msg = runCommand("status")
+	if isSuccess == false {
+		t.Error("unexpected results:", msg)
 	}
 }
