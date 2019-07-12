998c09a0cb55c2edc1c7affede8c5a011579a096
alfarisi <farisi_al@yahoo.com>
2019-07-12 22:35:03 +0700

    ganti map ke struct

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 	"strings"
 )
 
+type Identitas struct {
+	tipe, nomor string
+}
+
 var (
 	nLocker uint8
-	lockers []map[string]string
+	lockers []Identitas
 )
 
 func main() {
@@ -24,21 +28,22 @@ func main() {
 		
 		isSuccess, msg := runCommand(cmdString)
 		if isSuccess == false {
-			fmt.Fprintln(os.Stderr, msg)
+			fmt.Fprint(os.Stderr, msg, "\n\n")
+		} else {
+			fmt.Fprint(os.Stdout, "\n")
 		}
 	}
 }
 
 func runCommand(commandStr string) (bool, string) {
 	var msg string
-	commandStr = strings.TrimSuffix(commandStr, "\n")	
 	arrCommandStr := strings.Fields(commandStr)
 	
 	if len(arrCommandStr) < 1 {
 		msg = "Mohon tuliskan perintah.."
 		return false, msg
-	} else if (arrCommandStr[0] != "init") && (arrCommandStr[0] != "help") && (arrCommandStr[0] != "exit") && (nLocker == 0) {
-		msg = "Mohon tentukan jumlah locker terlebih dulu dengan perintah init [n].\n"
+	} else if arrCommandStr[0] != "init" && arrCommandStr[0] != "help" && arrCommandStr[0] != "exit" && nLocker == 0 {
+		msg = "Mohon tentukan jumlah locker terlebih dulu dengan perintah init [n]."
 		return false, msg
 	}
 	
@@ -48,7 +53,7 @@ func runCommand(commandStr string) (bool, string) {
 	
 	case "init":
 		if nLocker != 0 {
-			msg = fmt.Sprintf("Jumlah locker sudah ditentukan sebelumnya : %d \n", nLocker)
+			msg = fmt.Sprintf("Jumlah locker sudah ditentukan sebelumnya : %d", nLocker)
 			return false, msg
 		}
 		
@@ -59,37 +64,35 @@ func runCommand(commandStr string) (bool, string) {
 		
 		n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
 		
-		if (err != nil) || (n == 0) {
-			msg = "Error: jumlah locker harus bilangan positif dari 1 sampai 255"
+		if err != nil || n == 0 {
+			msg = "Jumlah locker harus bilangan positif dari 1 sampai 255"
 			return false, msg
 		}
 		
 		nLocker = uint8(n)
-		lockers = make([]map[string]string, nLocker)
-		
-		msg = "Success"
+		lockers = make([]Identitas, nLocker)
 		fmt.Fprintln(os.Stdout, "Berhasil membuat locker dengan jumlah", nLocker)
-		return true, msg
+		return true, "Success"
 	
 	case "input":
 		if len(arrCommandStr) < 3 {
-			msg = "Perintah ini memerlukan 2 argument : [KTP|SIM|Other] [nomor ID]"
+			msg = "Perintah ini memerlukan 2 argument : [KTP|SIM|Other] [nomor_ID]"
 			return false, msg
-		} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
-			msg = "Error: Tipe identitas yang dapat digunakan hanya KTP, SIM atau Other.\n"
+		} else if arrCommandStr[1] != "KTP" && arrCommandStr[1] != "SIM" && arrCommandStr[1] != "Other" {
+			msg = "Tipe identitas yang dapat digunakan hanya KTP, SIM atau Other."
 			return false, msg
 		}
 		
 		tLockerNum := findNomorIdentitas(arrCommandStr[2])
 		if tLockerNum != 0 {
-			msg = fmt.Sprintf("Peringatan: Kartu identitas dengan nomor %s sebelumnya sudah tersimpan di locker nomor %d \n", arrCommandStr[2], tLockerNum)
+			msg = fmt.Sprintf("Kartu identitas dengan nomor %s sebelumnya sudah tersimpan di locker nomor %d", arrCommandStr[2], tLockerNum)
 			return false, msg
 		}
 		
 		lockerNum := inputLocker(arrCommandStr[1], arrCommandStr[2])
 		
 		if lockerNum == 0 {
-			msg = "Maaf locker sudah penuh.\n"
+			msg = "Maaf locker sudah penuh."
 			return false, msg
 		} else {
 			msg = "Success"
@@ -100,41 +103,39 @@ func runCommand(commandStr string) (bool, string) {
 	case "status":
 		fmt.Fprintln(os.Stdout, "No. Locker   Tipe Identitas   No. Identitas")
 		
-		for k, val := range lockers {
-			if (val == nil) || (val["status"] == "kosong") {
+		for k, v := range lockers {
+			if v.tipe == "" || v.nomor == "" {
 				row := fmt.Sprintf("%9d.", k+1)
 				fmt.Fprintln(os.Stdout, row)
 			} else {
-				row := fmt.Sprintf("%9d.   %-14s   %s", k+1, val["tipeId"], val["nomorId"])
+				row := fmt.Sprintf("%9d.   %-14s   %s", k+1, v.tipe, v.nomor)
 				fmt.Fprintln(os.Stdout, row)
 			}
 		}
 		
-		msg = "Success"
-		return true, msg
+		return true, "Success"
 	
 	case "leave":
 		if len(arrCommandStr) < 2 {
-			msg = "Perintah ini memerlukan 1 argument : [nomor locker]"
+			msg = "Perintah ini memerlukan 1 argument : [nomor_locker]"
 			return false, msg
 		}
 		
 		n, err := strconv.ParseUint(arrCommandStr[1], 10, 8)
 		nl := uint8(n)
 		
-		if (err != nil) || (nl == 0) || (nl > nLocker) {
-			msg = "Error: Nomor locker tidak tersedia.\n"
+		if err != nil || nl == 0 || nl > nLocker {
+			msg = "Nomor locker tidak tersedia."
 			return false, msg
 		}
 		
-		lockers[nl-1] = map[string]string{"tipeId": "", "nomorId": "", "status": "kosong"}
-		msg = "Success"
+		lockers[nl-1] = Identitas{"", ""}
 		fmt.Fprintln(os.Stdout, "Loker nomor", n, "berhasil dikosongkan.")
-		return true, msg
+		return true, "Success"
 	
 	case "find":
 		if len(arrCommandStr) < 2 {
-			msg = "Perintah ini memerlukan 1 argument : [nomor identitas]"
+			msg = "Perintah ini memerlukan 1 argument : [nomor_identitas]"
 			return false, msg
 		}
 		
@@ -151,9 +152,9 @@ func runCommand(commandStr string) (bool, string) {
 	
 	case "search":
 		if len(arrCommandStr) < 2 {
-			msg = "Perintah ini memerlukan 1 argument : [tipe identitas]"
+			msg = "Perintah ini memerlukan 1 argument : [tipe_identitas]"
 			return false, msg
-		} else if (arrCommandStr[1] != "KTP") && (arrCommandStr[1] != "SIM") && (arrCommandStr[1] != "Other") {
+		} else if arrCommandStr[1] != "KTP" && arrCommandStr[1] != "SIM" && arrCommandStr[1] != "Other" {
 			msg = "Tipe identitas yang dapat digunakan hanya KTP, SIM dan Other"
 			return false, msg
 		}
@@ -161,9 +162,9 @@ func runCommand(commandStr string) (bool, string) {
 		arrNomorId := []string{}
 		isFound := false
 		
-		for _, val := range lockers {
-			if (val["tipeId"] == arrCommandStr[1]) && (val["status"] == "terisi") {
-				arrNomorId = append(arrNomorId, val["nomorId"])
+		for _, v := range lockers {
+			if v.tipe == arrCommandStr[1] {
+				arrNomorId = append(arrNomorId, v.nomor)
 				if isFound == false {
 					isFound = true
 				}
@@ -182,21 +183,19 @@ func runCommand(commandStr string) (bool, string) {
 		
 	case "help":
 		fmt.Fprintln(os.Stdout, "Daftar perintah yang bisa digunakan:\n\n init [jumlah_locker]\n status\n input [KTP|SIM|Other] [nomor_identitas]\n leave [nomor_locker]\n find [nomor_identitas]\n search [KTP|SIM|Other]\n help\n exit")
-		msg = "Success"
-		return true, msg
+		return true, "Success"
 		
 	default:
-		msg = "Perintah tidak dikenali. Gunakan 'help' untuk melihat daftar perintah yang dapat digunakan.\n"
+		msg = "Perintah tidak dikenali. Gunakan 'help' untuk melihat daftar perintah yang dapat digunakan."
 		return false, msg
 	}
 	
-	msg = "Success"
-	return true, msg
+	return false, "Unknown error"
 }
 
 func findNomorIdentitas(nomor string) uint8 {
-	for k, val := range lockers {
-		if (val["nomorId"] == nomor) && (val["status"] == "terisi") {
+	for k, v := range lockers {
+		if v.nomor == nomor {
 			return uint8(k+1)
 		}
 	}
@@ -204,9 +203,9 @@ func findNomorIdentitas(nomor string) uint8 {
 }
 
 func inputLocker(tipe, nomor string) uint8 {
-	for k, val := range lockers {
-		if (val == nil) || (val["status"] == "kosong") {
-			lockers[k] = map[string]string{"tipeId": tipe, "nomorId": nomor, "status": "terisi"}
+	for k, v := range lockers {
+		if v.tipe == "" && v.nomor == "" {
+			lockers[k] = Identitas{tipe, nomor}
 			return uint8(k+1)
 		}
 	}
